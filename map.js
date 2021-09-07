260cd6395b92c775fdc3abdd99ec4ff11563e1c2
Dinh Chau <dinhchau001@gmail.com>
2021-09-07 16:19:14 -0700

    Attempting leaflet map making

---
+++ map.js
@@ -0,0 +1,6 @@
+var map = L.map('map');
+
+L.tileLayer('https://mikesrpgcenter.com/zelda3/maps/lightworld_large.gif', {
+    attribution: '<a href="https://mikesrpgcenter.com/zelda3/maps/lightworld_large.gif"></a>',
+    maxZoom: 18, 
+}).addTo(map);
