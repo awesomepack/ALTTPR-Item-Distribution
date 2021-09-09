36121ed428786e679bf7c4f17549dc191f87a9e8
Dinh Chau <dinhchau001@gmail.com>
2021-09-08 19:01:44 -0700

    Trying to get map to appear

--- map.js
+++ map.js
@@ -1,6 +1,40 @@
-var map = L.map('map');
+var map;
 
-L.tileLayer('https://mikesrpgcenter.com/zelda3/maps/lightworld_large.gif', {
-    attribution: '<a href="https://mikesrpgcenter.com/zelda3/maps/lightworld_large.gif"></a>',
-    maxZoom: 18, 
+let lightworldlayer = L.tileLayer("map/LTTP-LightWorld.jpg",{
+    maxZoom: 4,
+    minZoom: 1, 
+    zoom: 1, 
+   });
+
+let darkworldlayer = L.tileLayer("map/LTTP-DarkWorld.png",{
+    maxZoom: 4,
+    minZoom: 1, 
+    zoom: 1, });
+
+let linkmap = {
+    "DarkWorld": darkworldlayer,
+    "LightWorld": lightworldlayer,
+};
+map = L.map('map', {
+  minZoom: 1,
+  maxZoom: 4,
+  center: [0, 0],
+  zoom: 1,
+  maxBoundsViscosity: 1,
+  crs: L.CRS.Simple,
+  layers: [darkworldlayer, lightworldlayer]
+});
+
+// var image = L.imageOverlay("map/LTTP-DarkWorld.png", [
+//   [0, 0],
+//   [432, 576]
+// ]);
+// image.addTo(map);
+L.control.layers(linkmap, {
+  collapsed: false
+}).addTo(map);
+
+map.setMaxBounds(new L.LatLngBounds([0, 0], [432, 576])); 
+L.tileLayer('', {
+  attribution: '&copy; <a href="https://mikesrpgcenter.com">Link to the Past</a>'
 }).addTo(map);
