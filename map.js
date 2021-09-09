266cfc4f7f449d5eafb787349f093be631d4b98d
Dinh Chau <dinhchau001@gmail.com>
2021-09-08 20:09:37 -0700

    I can't get both map to show up only one

--- map.js
+++ map.js
@@ -1,40 +1,61 @@
-var map;
+// var lightworldlayer = L.tileLayer("map/LTTP-LightWorld.jpg");
+// var darkworldlayer = L.tileLayer("map/LTTP-DarkWorld.png");
 
-let lightworldlayer = L.tileLayer("map/LTTP-LightWorld.jpg",{
-    maxZoom: 4,
-    minZoom: 1, 
-    zoom: 1, 
-   });
-
-let darkworldlayer = L.tileLayer("map/LTTP-DarkWorld.png",{
-    maxZoom: 4,
-    minZoom: 1, 
-    zoom: 1, });
-
-let linkmap = {
-    "DarkWorld": darkworldlayer,
-    "LightWorld": lightworldlayer,
-};
-map = L.map('map', {
+var map = L.map('map', {
   minZoom: 1,
   maxZoom: 4,
   center: [0, 0],
   zoom: 1,
   maxBoundsViscosity: 1,
   crs: L.CRS.Simple,
-  layers: [darkworldlayer, lightworldlayer]
 });
 
-// var image = L.imageOverlay("map/LTTP-DarkWorld.png", [
+
+
+
+// var map2 = L.map('map2', {
+//   minZoom: 1,
+//   maxZoom: 4,
+//   center: [0, 0],
+//   zoom: 1,
+//   maxBoundsViscosity: 1,
+//   crs: L.CRS.Simple,
+//   layer: [darkworldlayer]
+// });
+
+// map1.sync(map2);
+// map2.sync(map1);
+
+
+
+// document.getElementById('switch-layers').addEventHandler('click', function(ev){
+//   if (map.hasLayer(lightworldlayer)) {
+//       map.addLayer(darkworldlayer);
+//       map.removeLayer(lightworldlayer);} 
+//   else {
+//       map.addLayer(lightworldlayer);
+//       map.removeLayer(darkworldlayer);
+//   }
+// })
+// var overlaymap ={
+//   ""
+// }
+var image = L.imageOverlay("map/LTTP-DarkWorld.png", [
+  [0, 0],
+  [432, 576]
+]);
+image.addTo(map);
+
+// var image = L.imageOverlay("map/LTTP-LightWorld.jpg", [
 //   [0, 0],
 //   [432, 576]
 // ]);
 // image.addTo(map);
-L.control.layers(linkmap, {
-  collapsed: false
-}).addTo(map);
+// L.control.layers(linkmap, {
+//   collapsed: false
+// }).addTo(map);
 
-map.setMaxBounds(new L.LatLngBounds([0, 0], [432, 576])); 
-L.tileLayer('', {
-  attribution: '&copy; <a href="https://mikesrpgcenter.com">Link to the Past</a>'
-}).addTo(map);
+// map.setMaxBounds(new L.LatLngBounds([0, 0], [432, 576])); 
+// L.tileLayer('', {
+//   attribution: '&copy; <a href="https://mikesrpgcenter.com">Link to the Past</a>'
+// }).addTo(map);
