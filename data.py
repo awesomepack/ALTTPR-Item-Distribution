385b87da2e5358e01627fdd06c41aee6fe21fc00
Jason Engel <engel.jason@gmail.com>
2021-09-10 16:03:47 -0700

    blah

--- data.py
+++ data.py
@@ -96,6 +96,10 @@ session.close()
 # Session Closed
 ###################
 
+
+##########
+# Start Seed Data
+##########
 filepath = 'resources/seeds/alttpr_none_standard_ganon_bJqPVL0byeOBX2K.json'
 
 with open(filepath) as file:
@@ -116,24 +120,6 @@ bosses = seed_json.pop('Bosses')
 ###
 #
 ###
-
-
-# castle_tower = seed_json["Castle Tower"]
-# dark_palace = seed_json["Dark Palace"]
-# dark_overworld = seed_json["Dark World"]
-# death_mountain = seed_json["Death Mountain"]
-# desert_palace = seed_json["Desert Palace"]
-# eastern_palace = seed_json["Eastern Palace"]
-# ganons_tower = seed_json["Ganons Tower"]
-# hyrule_castle = seed_json["Hyrule Castle"]
-# ice_palace = seed_json["Ice Palace"]
-# light_overworld = seed_json["Light World"]
-# misery_mire = seed_json["Misery Mire"]
-# skull_woods = seed_json["Skull Woods"]
-# swamp_palace = seed_json["Swamp Palace"]
-# thieves_town = seed_json["Thieves Town"]
-# hera = seed_json["Tower Of Hera"]
-# turtle_rock = seed_json["Turtle Rock"]
 
 
 items = {}
