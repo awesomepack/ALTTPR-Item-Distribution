27725a39096471277dd64dac45c5b0e99def9c6f
Jason Engel <engel.jason@gmail.com>
2021-09-11 16:36:34 -0700

    no longer able to scroll off the map

--- map.js
+++ map.js
@@ -1,19 +1,22 @@
-
+const corner1 = L.latLng(0, 0)
+const corner2 = L.latLng(2007, 4014)
+const bounds = L.latLngBounds(corner1, corner2)
 var map = L.map('map', {
   minZoom: -2,
   maxZoom: 2,
-  center: [1003, 2006],
+  center: bounds.getCenter(),
   zoom: -2,
+  maxBounds: bounds,
   maxBoundsViscosity: 1,
   crs: L.CRS.Simple,
 });
 
-var bounds = [[0,0], [2007,2007]]
-var image = L.imageOverlay('resources/images/lttp_lightworld.png', bounds)
-image.addTo(map)
+var i1bounds = [[0,0], [2007,2007]]
+var image1 = L.imageOverlay('resources/images/lttp_lightworld.png', i1bounds)
+image1.addTo(map)
 
-var bounds2 = [[0,2007], [2007,4014]]
-var image2 = L.imageOverlay('resources/images/lttp_darkworld.png', bounds2)
+var i2bounds = [[0,2007], [2007,4014]]
+var image2 = L.imageOverlay('resources/images/lttp_darkworld.png', i2bounds)
 image2.addTo(map)
 
 // d3.json('http://localhost:5000/viz1').then(function(data) {
