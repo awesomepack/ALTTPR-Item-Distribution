c83795b352126a048316f48dbe8bdf1a7db7da21
Dinh Chau <dinhchau001@gmail.com>
2021-09-11 16:15:14 -0700

    Changed markers

--- map.js
+++ map.js
@@ -16,17 +16,35 @@ var bounds2 = [[0,2007], [2007,4014]]
 var image2 = L.imageOverlay('resources/images/lttp_darkworld.png', bounds2)
 image2.addTo(map)
 
+// d3.json('http://localhost:5000/viz1').then(function(data) {
+//   for (var i = 0; i < data.length; i++) {
+//     marker = new L.marker(data[i][1])
+//       .bindPopup(data[i][0]).addTo(map);}
+
 d3.json('http://localhost:5000/viz1').then(function(data) {
   for (var i = 0; i < data.length; i++) {
-    marker = new L.marker(data[i][1])
-      .bindPopup(data[i][0]).addTo(map);}
-});
+    marker = new L.circleMarker(data[i][1])
+    .bindPopup(data[i][0]).addTo(map);
+marker.setStyle({radius: '6'})
+
+
+// var circle = L.circleMarker(mapstyleInfo(feature) {
+//   return {
+//     opacity: 1,
+//     fillOpacity: 1,
+//     fillColor: "#F002134",
+//     color: "#000000",
+//     radius: 10,
+//     stroke: true,
+//     weight: 0.5
+//   }
+// }
+// ).addTo(map)
+
+
+  
 
 
 
 
-
-
-
-
-
+  }})
