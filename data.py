2b870bb7da247ae2ab2975b97338c63162f13e30
Jason Engel <engel.jason@gmail.com>
2021-09-11 01:25:39 -0700

    fixed fix for bug

--- data.py
+++ data.py
@@ -117,20 +117,20 @@ def loadAllData(folderpath : str, limit = -1, batchsize = 1000, maketables = Tru
         makeTables(dropfirst=dropfirst)
     if limit <=0:
         for i, file in enumerate(os.scandir(folderpath)):
-            if i > 0 and batchsize > 0 and i%batchsize == 0:
-                session.commit()
             print (f'unlimited ({i}): {file.path}')
             if file.path.endswith('.json'):
                 loadSeed(file.path, session)
+                if i > 0 and batchsize > 0 and i%batchsize == 0:
+                    session.commit()
     else:
         for i, file in enumerate(os.scandir(folderpath)):
             if i>=limit:
                 break
-            if i > 0 and batchsize > 0 and i%batchsize == 0:
-                session.commit()
             print (f'{limit-i}: {file.path}' )
             if file.path.endswith('.json'):
                 loadSeed(file.path, session)
+                if i > 0 and batchsize > 0 and i%batchsize == 0:
+                    session.commit()
     session.commit()
     session.close()
     
