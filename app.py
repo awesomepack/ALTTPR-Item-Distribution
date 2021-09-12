a052ef33d7386987c3ba3b5a6b3afaf5343919ac
Jason Engel <engel.jason@gmail.com>
2021-09-11 20:02:02 -0700

    only load metadata once

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import func
 from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.sql.schema import MetaData
 from login import postgres_password , postgres_username
 from flask import Flask , redirect , url_for , request , render_template
@@ -19,51 +20,50 @@ app = Flask(__name__)
 # Creating connection to alttpr database
 db_string = f'postgresql://{postgres_username}:{postgres_password}@localhost/alttpr'
 engine = create_engine(db_string , echo = True)
+Session = sessionmaker(bind=engine)
+locationMetadata = []
 
+def getLocationMetadata():
+    if (len(locationMetadata)==0):
+        session = Session()
+        # Select all entries from the database
+        # text_statement = text("SELECT * FROM public.\"location-metadata\" ORDER BY location ASC ")
+        statement = select([tables.LocationMetadata.location, tables.LocationMetadata.x, tables.LocationMetadata.y, tables.LocationMetadata.map, tables.LocationMetadata.count])
+        results = session.execute(statement)
+        
+        # populating mapLocations with results from query
+        for row in results:
+            location = row[0]
+            map = row[3]
+            if map == "lightworld":
+                x = row[1]
+                y = 2007 - row[2]
+                coords = [y, x]
+            else:
+                x = row[1] + 2007
+                y = 2007 - row[2]
+                coords = [y, x]
+                
+            count = row[4]
+            locationMetadata.append([location, coords, count])
+        
+        session.close()
+    return locationMetadata
 
 @app.after_request
 def add_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
+# Function to render potential home page with information on API routes
 @app.route('/ALTTPR')
 def home():
     return 'This is the home page'
 
-    # Function to render potential home page with information on API routes
-
-
+    
 @app.route('/viz1')
 def query_viz1():
-
-    # Begin Session
-    session = Session(engine)
-
-    # Select all entries from the database
-    # text_statement = text("SELECT * FROM public.\"location-metadata\" ORDER BY location ASC ")
-    statement = select([tables.LocationMetadata.location, tables.LocationMetadata.x, tables.LocationMetadata.y, tables.LocationMetadata.map, tables.LocationMetadata.count])
-    results = session.execute(statement)
-    
-
-    # initializing the map locations dictionary that will be served
-    mapLocations = []
-
-    # populating mapLocations with results from query
-    for row in results:
-        location = row[0]
-        map = row[3]
-        if map == "lightworld":
-            x = row[1]
-            y = 2007 - row[2]
-            coords = [y, x]
-        else:
-            x = row[1] + 2007
-            y = 2007 - row[2]
-            coords = [y, x]
-            
-        count = row[4]
-        mapLocations.append([location, coords, count])
-    return jsonify(mapLocations)
+    return jsonify(getLocationMetadata())
 
 
 @app.route('/viz2/<location_name>')
@@ -91,14 +91,11 @@ def query_viz2(location_name):
     return jsonify(locationItems)
 
 
-
-
-
 @app.route('/viz3')
 def query_viz3():
-    
-    if __name__ == '__main__':
+    pass
 
-        app.run( debug = True)
+if __name__ == '__main__':
+    app.run( debug = True)
 
 
